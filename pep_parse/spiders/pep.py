6a051d00d953d7557b15c4132482e81fb64c241b
Евгений Братанов <sonik.zhenyaa@gmail.com>
2024-12-16 18:18:03 +0300

    Pep1

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -5,13 +5,12 @@ from pep_parse.items import PepParseItem
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
-    start_urls = [f'https://{allowed_domains[0]}/']
+    start_urls = [f'https://{domain}/' for domain in allowed_domains]
 
     def parse(self, response):
-        pep_links = response.xpath(
+        for href in response.xpath(
             '//section[@id="numerical-index"]//table//tr//a/@href'
-        ).getall()
-        for href in pep_links:
+        ).getall():
             yield response.follow(href, callback=self.parse_pep)
 
     def parse_pep(self, response):
